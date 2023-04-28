8515e71afd1037783b8eb961d2af93c70e8c9d39
Galih Wicaksono <galihwicaksono90@gmail.com>
2023-04-28 18:47:14 +0700

    WIP

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,14 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="bg-primary">
-      <div className="container mx-auto text-white flex items-center h-[105px]">
+      <div
+        className="container mx-auto text-white flex items-center h-[105px]"
+        data-cy="header-background"
+      >
         <Link href="/">
-          <h2 className="text-2xl font-bold">TO DO LIST APP</h2>
+          <h2 className="text-2xl font-bold" data-cy="header-title">
+            TO DO LIST APP
+          </h2>
         </Link>
       </div>
     </header>
